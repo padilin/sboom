c99502299c7851891b1cd1ad42cafab682b512ab
padilin <tj.pyro@gmail.com>
2025-11-07 21:24:10 -0600

    Remove commented-out struct definitions for Dependency, Repo, and Release

--- database/structs.go
+++ database/structs.go
@@ -4,32 +4,6 @@ import (
 	"gorm.io/gorm"
 )
 
-// type Dependency struct {
-// 	CreatedAt time.Time
-// 	UpdatedAt time.Time
-// 	DeletedAt gorm.DeletedAt
-// 	Name string `gorm:"primaryKey" json:"name"`
-// 	Version string `gorm:"primaryKey" json:"version"`
-// 	Releases []*Release `gorm:"many2many:release_dependencies;"`
-// 	Dependencies  []*Dependency `gorm:"many2many:dependency_relations" json:"dependencies"`
-// }
-
-// type Repo struct {
-// 	gorm.Model
-// 	Name string `gorm:"not null" json:"name"`
-// 	Url *string
-// 	Path *string
-// 	Releases []Release `gorm:"foreignKey:RepoID"`
-// }
-
-// type Release struct {
-// 	gorm.Model
-// 	Version string
-// 	Commit string
-// 	Dependencies []*Dependency `gorm:"many2many:release_dependencies"`
-// 	RepoID uint
-// }
-
 type Dependency struct {
 	gorm.Model
 	// Keep numeric ID as the primary key and enforce uniqueness on (Name,Version)
