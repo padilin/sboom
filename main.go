95f5ccaa001c556b2ea551f5f5354a5db1ffc516
padilin <tj.pyro@gmail.com>
2025-11-07 21:59:15 -0600

    Added nested example to main.go.

--- main.go
+++ main.go
@@ -5,40 +5,45 @@ import (
 	"sboom/database"
 )
 
-
 func main() {
-    database.InitDB("sboom.db")
-    repo := database.Repo{
-        Name: "tester",
-        Releases: []*database.Release{
-            {
-                Version: "1.0.0",
-                Dependencies: []*database.Dependency{
-                    {
-                        Name:    "GORM",
-                        Version: "1",
-                    },
-                    {
-                        Name:    "Git",
-                        Version: "1",
-                    },
-                },
-            },
-            {
-                Version: "2.0.0",
-                Dependencies: []*database.Dependency{
-                    {
-                        Name:    "GORM",
-                        Version: "2",
-                    },
-                    {
-                        Name:    "Git",
-                        Version: "1",
-                    },
-                },
-            },
-        },
-    }
+	database.InitDB("sboom.db")
+	repo := database.Repo{
+		Name: "tester",
+		Releases: []*database.Release{
+			{
+				Version: "1.0.0",
+				Dependencies: []*database.Dependency{
+					{
+						Name:    "GORM",
+						Version: "1",
+					},
+					{
+						Name:    "Git",
+						Version: "1",
+						Related: []*database.Dependency{
+							{
+								Name:    "subLib",
+								Version: "100",
+							},
+						},
+					},
+				},
+			},
+			{
+				Version: "2.0.0",
+				Dependencies: []*database.Dependency{
+					{
+						Name:    "GORM",
+						Version: "2",
+					},
+					{
+						Name:    "Git",
+						Version: "1",
+					},
+				},
+			},
+		},
+	}
 
 	savedRepo, err := database.SaveRepo(repo)
 	if err != nil {
@@ -46,4 +51,3 @@ func main() {
 	}
 	log.Println(savedRepo.CreatedAt)
 }
-
